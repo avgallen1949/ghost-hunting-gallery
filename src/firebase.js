4460b147929da477666ef4e7aef96733dbcb47a3
avgallen1949 <138723887+avgallen1949@users.noreply.github.com>
2025-10-01 20:57:04 -0400

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
-// REPLACE WITH YOUR FIREBASE CONFIG FROM STEP 3 ABOVE
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBKQyWn8RlQ47f1PA9BQSP1OiIG6dON6XU",
+  authDomain: "ghost-hunting-gallery.firebaseapp.com",
+  databaseURL: "https://ghost-hunting-gallery-default-rtdb.firebaseio.com",
+  projectId: "ghost-hunting-gallery",
+  storageBucket: "ghost-hunting-gallery.firebasestorage.app",
+  messagingSenderId: "486607492909",
+  appId: "1:486607492909:web:57a9e8c026e48bb1bb25a4"
 };
 
 const app = initializeApp(firebaseConfig);
